abe4b54d8d779e7d041a0d4b405290cd372e56a5
Aleksei Konstantinov <mralexey111@gmail.com>
2024-04-25 10:59:40 +0300

    Task18 done

---
+++ Task18/src/main/java/ru/sonder/task18/DTOs/PostDTO.java
@@ -0,0 +1,17 @@
+package ru.sonder.task18.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+    private Long id;
+    private String text;
+    private LocalDate creationDate;
+    private Long userId;
+}
---
+++ Task18/src/main/java/ru/sonder/task18/configs/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.sonder.task18.configs;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "ru.sonder.task18.repositories")
+@EntityScan(basePackages = "ru.sonder.task18.models")
+@ComponentScan(basePackages = "ru.sonder.task18")
+public class AppConfig {}
